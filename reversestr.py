7a9e94d83d4303b5989b081f24ee691aeadc82ca
VivasvanVerma <vvofficial.yt@gmail.com>
2025-04-21 16:19:36 +0530

    string

---
+++ reversestr.py
@@ -0,0 +1,9 @@
+string = input("Enter an all lowercase string: ")
+string2 = ""
+
+for i in string:
+    string2 = i + string2
+
+print("\n The original string: ", string)
+print("\n The reversed string: ", string2)
+
